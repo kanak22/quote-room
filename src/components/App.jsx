d6f35143b907d4281a3b0240bb4e0292feed0996
Kanak <kanakkangdan22@gmail.com>
2021-09-30 13:42:24 +0530

    Added files

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+
+function App() {
+ //code for app
+}
+
+export default App;
---
+++ src/components/Entry.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Entry() {
+ //code for quote entries.
+}
+
+export default Entry;
